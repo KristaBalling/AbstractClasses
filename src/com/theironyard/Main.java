5adb335f712bce8785972aaa2a2dbdb62e7a39e4
Krista Blachian <krista.blachian@scifusion360.com>
2018-11-20 16:22:16 -0500

    searches and deletes nodes

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     MyLinkedList list = new MyLinkedList(null);
     list.traverse(list.getRoot());
-	String stringData = "Darwin Brisbane Perth Melbourne Canberra Adelaide Sydney Canberra";
+	String stringData = "5 7 3 9 8 2 1 0 4 6";
 
 	String[] data = stringData.split(" ");
 	for(String s : data) {
@@ -14,6 +14,28 @@ public class Main {
 
     }
 
-    list.traverse(list.getRoot());
+
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("3"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("5"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("0"));
+        list.removeItem(new Node("4"));
+        list.removeItem(new Node("2"));
+        list.traverse(list.getRoot());
+
+        list.removeItem(new Node("9"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("8"));
+        list.traverse(list.getRoot());
+        list.removeItem(new Node("6"));
+        list.traverse(list.getRoot());
+        list.removeItem(list.getRoot());
+        list.traverse(list.getRoot());
+        list.removeItem(list.getRoot());
+        list.traverse(list.getRoot());
     }
 }
